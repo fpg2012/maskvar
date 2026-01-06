f022051348d0523f10c2afabebc54fdc52449f8c
nth233 <fpg2012@foxmail.com>
2026-01-07 00:13:00 +0800

    try different lr

---
+++ train_scripts/simple_var/train_simple_var_v0_lr1e-3_bs32.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEVICE=${1:-cuda}
+OUTDIR=${2:-out/simple_var_v0_lr1e-3_bs32}
+
+python train_scripts/train_simple_var.py \
+    --device $DEVICE \
+    --outdir $OUTDIR \
+    --lr 1e-3 \
+    --batch_size 16 \
+    --accumulate_steps 2 \
+    --outer_iters 40 \
+    --val_iters 64 \
+    --inner_iters 10000
